926ad4e7a1adabca65da287e8fb2e22fee0eb7b9
Yi Ting Seah <k.seahyt@gmail.com>
2016-08-01 11:01:20 +0800

    move img in  home.html.erb into jumbotron, and updated a bit of users_controlle

--- redmart-rails-me/app/controllers/users_controller.rb
+++ redmart-rails-me/app/controllers/users_controller.rb
@@ -1,13 +1,18 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def show
+    @post = User.posts
   end
 
   def new
+    @user = User.new
   end
 
   def edit
   end
+
+  
 end
