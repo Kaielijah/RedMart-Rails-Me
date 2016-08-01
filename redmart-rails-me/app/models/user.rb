2d858f9402cd2a9bde172cc5bf6f427bffcab2dd
Yi Ting Seah <k.seahyt@gmail.com>
2016-08-01 18:26:41 +0800

    added validation for users and email

--- redmart-rails-me/app/models/user.rb
+++ redmart-rails-me/app/models/user.rb
@@ -1,3 +1,22 @@
 class User < ApplicationRecord
 has_many :reviews
+
+before_save { self.email.downcase! }
+
+  # VALIDATION
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :name,
+            presence: true,
+            length: { maximum: 50, message: 'You have exceeded the maximum about of characters' }
+
+  validates :email,
+            presence: true,
+            length: { maximum: 50 },
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
+
+
+  # FOR HASHING PASSWORD
+  has_secure_password
 end
