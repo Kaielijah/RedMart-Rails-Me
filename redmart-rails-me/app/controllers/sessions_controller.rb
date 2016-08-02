eaddf25796e362c4971220db1cc6d11217a1083a
Yi Ting Seah <k.seahyt@gmail.com>
2016-08-02 22:26:20 +0800

    Sign up view with partial, edit view with partial, authorization, friendly forwarding, showing all users,

--- redmart-rails-me/app/controllers/sessions_controller.rb
+++ redmart-rails-me/app/controllers/sessions_controller.rb
@@ -7,12 +7,14 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
-      redirect_to user
+      params[:session][:rememeber_me] == '1' ? remember(user) : forget(user)
+      redirect_back_to user
     else
       flash[:danger] = 'Invalid email/password combination'
     render 'new'
+    end
   end
-end
+
 
 def destroy
    log_out
