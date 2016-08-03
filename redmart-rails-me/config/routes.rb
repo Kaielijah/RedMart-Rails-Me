994c19a07ff46c37ff977b03e743dfc87218f18d
Yi Ting Seah <k.seahyt@gmail.com>
2016-08-03 22:12:27 +0800

    remove extra resources

--- redmart-rails-me/config/routes.rb
+++ redmart-rails-me/config/routes.rb
@@ -35,9 +35,7 @@ Rails.application.routes.draw do
   resources :products do
     resources :microposts
   end
-  resources :microposts do
-    resources :products
-  end
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
