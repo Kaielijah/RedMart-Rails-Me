0c88c5d84139c2d4f4fc1429a74e01a308f3b4fb
Yi Ting Seah <k.seahyt@gmail.com>
2016-08-03 12:30:19 +0800

    added product controller

--- redmart-rails-me/config/routes.rb
+++ redmart-rails-me/config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
 
 
+  get 'products/index'
+
+  get 'products/show'
+
+  get 'products/new'
+
+  get 'products/edit'
+
   root 'static_pages#home'
 
   # static pages routes, not connected to models
