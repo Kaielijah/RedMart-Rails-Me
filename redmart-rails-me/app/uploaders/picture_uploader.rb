d3f17cfbf18e8a02d89c7b4a5bf67e9c157ba15d
Yi Ting Seah <k.seahyt@gmail.com>
2016-08-04 18:35:18 +0800

    modiy css container

--- redmart-rails-me/app/uploaders/picture_uploader.rb
+++ redmart-rails-me/app/uploaders/picture_uploader.rb
@@ -2,8 +2,8 @@ class PictureUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
     process resize_to_limit: [400, 400]
 
-storage :fog
-  
+storage :file
+
 # Override the directory where uploaded files will be stored.
 # This is a sensible default for uploaders that are meant to be mounted:
 def store_dir
